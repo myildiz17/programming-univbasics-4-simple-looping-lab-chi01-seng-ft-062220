8e24b593d061b6dd48da9a6189755355f2a1cdb7
Muhammet Yildiz <muhammetyildiz@gmail.com>
2020-04-21 04:10:32 +0000

    Done.

---
+++ test.rb
@@ -0,0 +1,10 @@
+# Write your methods heredef
+def loop_message_five_times (message)
+  count = 0
+  while count < 5 do
+    puts message
+    count += 1
+  end
+end
+
+loop_message_five_times("selam")
